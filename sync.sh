f05fc9f359a0f2b929e766541df28f298ab97fa5
Your Name <you@example.com>
2017-02-03 09:19:14 +0100

    Cleanup command did not revert file changes, added revert command.

--- sync.sh
+++ sync.sh
@@ -4,8 +4,9 @@ do
 	
 	if [ ! -z "$(ls -A $SYNC_DIR)" ] && [ ! -z "$(svn --username "$SYNC_USER" --password "$SYNC_PASS" status -u "$SYNC_DIR" | grep -v Status)" ]; then
 		date
-		svn --username "$SYNC_USER" --password "$SYNC_PASS" update "$SYNC_DIR"
+		svn --username "$SYNC_USER" --password "$SYNC_PASS" revert -R "$SYNC_DIR"
 		svn --username "$SYNC_USER" --password "$SYNC_PASS" cleanup --remove-unversioned "$SYNC_DIR"
+		svn --username "$SYNC_USER" --password "$SYNC_PASS" update "$SYNC_DIR"
 	fi
 	
 	sleep "$SYNC_INT"
