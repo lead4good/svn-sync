a33fa2a99a560b668662b0e0d2303f095780111f
Your Name <you@example.com>
2017-05-15 12:36:37 +0200

    sync fix 3

--- sync.sh
+++ sync.sh
@@ -2,13 +2,21 @@
 while [ 1 ]
 do
 
-	if [ ! -z "$(ls -A $SYNC_DIR)" ] && [ ! -z "$(svn --username "$SYNC_USER" --password "$SYNC_PASS" status -u "$SYNC_DIR" | grep -v Status)" ]; then
-		date
-		svn --username "$SYNC_USER" --password "$SYNC_PASS" revert -R "$SYNC_DIR"
-		if ! [ "$ALLOW_OVERWRITE" = "true" ]; then
-			svn --username "$SYNC_USER" --password "$SYNC_PASS" cleanup --remove-unversioned "$SYNC_DIR"
+	if [ ! -z "$(ls -A $SYNC_DIR)" ]
+	  if ! [ "$ALLOW_OVERWRITE" = "true" ]; then
+			STATUS="$(svn --username "$SYNC_USER" --password "$SYNC_PASS" status -u "$SYNC_DIR" | grep -v Status)"
+		else
+			STATUS="$(svn --username "$SYNC_USER" --password "$SYNC_PASS" status -u "$SYNC_DIR" | grep -v Status | grep -v ?)"
 		fi
-		svn --username "$SYNC_USER" --password "$SYNC_PASS" update "$SYNC_DIR"
+
+		if ! [ -z "$STATUS"]; then
+			date
+			svn --username "$SYNC_USER" --password "$SYNC_PASS" revert -R "$SYNC_DIR"
+			if ! [ "$ALLOW_OVERWRITE" = "true" ]; then
+				svn --username "$SYNC_USER" --password "$SYNC_PASS" cleanup --remove-unversioned "$SYNC_DIR"
+			fi
+			svn --username "$SYNC_USER" --password "$SYNC_PASS" update "$SYNC_DIR"
+		fi
 	fi
 
 	sleep "$SYNC_INT"
