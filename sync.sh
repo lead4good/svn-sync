8685e1976f68add215eab453fbdc42dc09a86519
Your Name <you@example.com>
2017-05-15 12:06:00 +0200

    sync fix

--- sync.sh
+++ sync.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 while [ 1 ]
 do
-	
+
 	if [ ! -z "$(ls -A $SYNC_DIR)" ] && [ ! -z "$(svn --username "$SYNC_USER" --password "$SYNC_PASS" status -u "$SYNC_DIR" | grep -v Status)" ]; then
 		date
+		svn --username "$SYNC_USER" --password "$SYNC_PASS" revert -R "$SYNC_DIR"
 		if ! [ "$ALLOW_OVERWRITE"=="true" ]; then
-			svn --username "$SYNC_USER" --password "$SYNC_PASS" revert -R "$SYNC_DIR"
 			svn --username "$SYNC_USER" --password "$SYNC_PASS" cleanup --remove-unversioned "$SYNC_DIR"
 		fi
 		svn --username "$SYNC_USER" --password "$SYNC_PASS" update "$SYNC_DIR"
 	fi
-	
+
 	sleep "$SYNC_INT"
 done
