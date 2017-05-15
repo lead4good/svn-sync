a323815f38ea6a4cc272715a4d63cccb83b2231c
Your Name <you@example.com>
2017-05-15 12:47:19 +0200

    sync fix 5

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if ! [ "$ALLOW_OVERWRITE" = "true" ] || [ -z "$(ls -A $SYNC_DIR)" ]; then
+if [ "$ALLOW_OVERWRITE" = "true" ] || [ -z "$(ls -A $SYNC_DIR)" ]; then
 	svn --username "$SYNC_USER" --password "$SYNC_PASS" --force checkout "$SYNC_URL" "$SYNC_DIR"
 fi
 
